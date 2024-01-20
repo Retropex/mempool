5da64f74e30f141aa28e8bc15b20b43006e3a389
Léo Haf <leohaf@orangepill.ovh>
2024-01-20 12:34:26 +0100

    change color spam

--- frontend/src/app/components/block-overview-graph/utils.ts
+++ frontend/src/app/components/block-overview-graph/utils.ts
@@ -44,8 +44,8 @@ export const defaultMarginalFeeColors = defaultFeeColors.map((color) => darken(d
 export const defaultAuditColors = {
   censored: hexToColor('f344df'),
   missing: darken(desaturate(hexToColor('f344df'), 0.3), 0.7),
-  added: hexToColor('ff0000'),
-  selected: darken(desaturate(hexToColor('ff0000'), 0.3), 0.7),
+  added: hexToColor('9310ea'),
+  selected: darken(desaturate(hexToColor('9310ea'), 0.3), 0.7),
   accelerated: hexToColor('8F5FF6'),
 };
 
