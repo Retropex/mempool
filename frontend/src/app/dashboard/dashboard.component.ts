face3bf1f8ab6c43a0cab3f57eae9347b12c686c
Léo Haf <leo@haf.ovh>
2025-08-27 10:28:30 +0000

    add Knots progression bar

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,7 @@ import { WebsocketService } from '@app/services/websocket.service';
 import { SeoService } from '@app/services/seo.service';
 import { ActiveFilter, FilterMode, GradientMode, toFlags } from '@app/shared/filters.utils';
 import { detectWebGL } from '@app/shared/graphs.utils';
+import { BitnodesService } from '@app/services/bitnodes.service';
 
 interface MempoolBlocksData {
   blocks: number;
@@ -61,6 +62,9 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
   expiredUtxos$: Observable<FederationUtxo[]>;
   emergencySpentUtxosStats$: Observable<any>;
   fullHistory$: Observable<any>;
+  knotsPercentage: number = 0;
+  totalKnotsNodes: number = 0;
+  totalBitcoinNodes: number = 0;
   isLoad: boolean = true;
   filterSubscription: Subscription;
   mempoolInfoSubscription: Subscription;
@@ -92,6 +96,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
     private apiService: ApiService,
     private websocketService: WebsocketService,
     private seoService: SeoService,
+    private bitnodesService: BitnodesService,
     @Inject(PLATFORM_ID) private platformId: Object,
   ) {
     this.webGlEnabled = this.stateService.isBrowser && detectWebGL();
@@ -417,6 +422,13 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
 
     this.currencySubscription = this.stateService.fiatCurrency$.subscribe((fiat) => {
       this.currency = fiat;
+    });
+
+    // Load Knots nodes data
+    this.bitnodesService.getKnotsNodeDistribution().subscribe(knotsData => {
+      this.knotsPercentage = knotsData.totals.percentageOfTotal;
+      this.totalKnotsNodes = knotsData.totals.totalNodes;
+      this.totalBitcoinNodes = Math.round(this.totalKnotsNodes / (this.knotsPercentage / 100));
     });
   }
 
