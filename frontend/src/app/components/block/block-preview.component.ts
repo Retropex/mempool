920fe052808dc7916627bf8f641a1f05ed651843
Léo Haf <leo@haf.ovh>
2025-10-08 14:02:13 +0200

    new preview layout

--- frontend/src/app/components/block/block-preview.component.ts
+++ frontend/src/app/components/block/block-preview.component.ts
@@ -30,6 +30,7 @@ export class BlockPreviewComponent implements OnInit, OnDestroy {
   error: any;
   blockSubsidy: number;
   fees: number;
+  health: number;
   overviewError: any = null;
 
   overviewSubscription: Subscription;
@@ -63,6 +64,7 @@ export class BlockPreviewComponent implements OnInit, OnDestroy {
         this.error = undefined;
         this.overviewError = undefined;
         this.fees = undefined;
+        this.health = undefined;
 
         let isBlockHeight = false;
         if (/^[0-9]+$/.test(blockHash)) {
@@ -111,6 +113,9 @@ export class BlockPreviewComponent implements OnInit, OnDestroy {
         this.setBlockSubsidy();
         if (block?.extras?.reward !== undefined) {
           this.fees = block.extras.reward / 100000000 - this.blockSubsidy;
+        }
+        if (block?.extras?.matchRate !== undefined) {
+          this.health = block.extras.matchRate;
         }
         this.stateService.markBlock$.next({ blockHeight: this.blockHeight });
         this.isLoadingOverview = true;
